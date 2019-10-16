24d35299c6c56ecc60e4522d88dbb4da7c571ab8
bananabread8 <56650263+bananabread8@users.noreply.github.com>
2019-10-16 13:33:45 -0400

    Initial commit

---
+++ src/views/Pets.vue
@@ -0,0 +1,53 @@
+<template>
+  <v-container grid-list-md fluid>
+    <v-layout wrap>
+      <v-flex xs12 sm4 md3 v-for="pet in dogs" :key="pet.breed">
+        <appdog :dog="pet" @addToFavorites="addToFavorites"></appdog>
+      </v-flex>
+    </v-layout>
+  </v-container>
+</template>
+
+<script>
+import axios from "axios";
+import { Dogs } from "../data/dogs";
+import Dog from "../components/Dog.vue";
+import { mapActions } from "vuex";
+
+axios.defaults.baseURL = "https://dog.ceo/api";
+
+export default {
+  components: {
+    appdog: Dog
+  },
+  data() {
+    return {
+      dogs: Dogs
+    };
+  },
+  created() {
+    this.dogs.forEach(dog => {
+      dog.img = "";
+    });
+    const linksArray = this.dogs.map(
+      dog => "/breed/" + dog.breed + "/images/random"
+    );
+    axios.all(linksArray.map(link => axios.get(link))).then(
+      axios.spread((...res) => {
+        this.dogs.forEach((dog, index) => {
+          dog.img = res[index].data.message;
+        });
+      })
+    );
+  },
+  methods: {
+    ...mapActions(["addToFavorites"])
+  }
+};
+</script>
+
+<style scoped>
+p {
+  margin: 0;
+}
+</style>
